a7678ff511df382c4db2f7abb53a2f5f8249f199
rajnishpandey1 <rajnishspandey1@gmail.com>
2023-10-22 14:51:13 +0530

    'basics'

--- 08DataStructure_tuple.py
+++ 08DataStructure_tuple.py
@@ -35,13 +35,23 @@ print(len(Tuple2))
 numbers = (1,3,4,5,6,3,2,1,0,10,12,4,15,34,20)
 print(numbers)
 
-print(sorted(numbers))
+print(sorted(numbers)) #output will be in the form of list
 
 #nested Tuple
 
-Tuple3 = (Tuple, Tuple2,(4,5,6,7,))
+Tuple3 = (Tuple, Tuple2,(4,5,6,7,(1,2,3)))
 print(Tuple3)
 
 
 #indexing on Nested Tuple
+
+print('Element 0 of Tuple :',Tuple3[0])
+print('Element 2 of Tuple :',Tuple3[2])
+print('Element 0, 0 of Tuple :',Tuple3[0][2])
+print('Element 2, 2 of Tuple :',Tuple3[2][2])
+print('Element 2, 4,1 of Tuple :',Tuple3[2][4][1])
+
+#find the index of element
+element = ("Hello","It's me","can you hear me")
+print("Index of element :",element.index("It's me"))
 
--- 09DataStructure_List.py
+++ 09DataStructure_List.py
@@ -1,7 +1,24 @@
-List = ["a","b","c"]
+List = ['a','b','c']
 print(List)
 
 #Find the first two elements of the list 
 print(List[0:2])
 #Change the first element of the list to an uppercase 
 print(List[0].upper())
+
+
+#slicing
+
+print(List[1])
+
+List2 = ["Jordan","Java","Python","C++"]
+List3 = ["Aadm", "Gilchrist","Mathew", "Haden",List]
+List4 = [List,List2,List3,]
+print(List4)
+print(List4[1][2])
+
+#extend
+
+extended = List.extend([1])
+print(extended)
+
