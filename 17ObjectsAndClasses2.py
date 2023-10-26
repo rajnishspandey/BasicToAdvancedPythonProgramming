3ebc3d53652c1d92cb42fb94b1bcded352039322
rajnishspandey <pandeyrajnish86@yahoo.com>
2023-10-26 22:20:38 +0530

    Advanced

--- 17ObjectsAndClasses2.py
+++ 17ObjectsAndClasses2.py
@@ -1,1 +1,15 @@
-testing
+# Import the library
+class Circle(object):
+    
+    def __init__(self,radius = 3, colour ='Green'):
+        self.radius = radius
+        self.colour = colour
+
+    #method
+    def add_radius(self,r):
+        self.radius = self.radius + r
+        return (self.radius)
+    
+RedCircle = Circle(10,"Red")
+print(f"radius is {RedCircle.radius} and colour of the circle is {RedCircle.colour}")
+
