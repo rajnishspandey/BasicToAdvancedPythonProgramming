ba22149d7b9c0d7c7d2d3529fd08a9aeaec080e8
rajnishspandey <pandeyrajnish86@yahoo.com>
2023-10-27 19:21:04 +0530

    Advanced

--- 27FileHandling8.py
+++ 27FileHandling8.py
@@ -1,12 +1,13 @@
 #'r' - read only
-#'w' - write only
+#'w' - write only -- writes from first position and deletes all other existing data
+#'a' - append -- appends the new data on the existing record
 #'r+' -  Reading and writing. Cannot truncate the file.
 #'w+' - Writing and reading. Truncates the file.
 #'a+' : Appending and Reading. Creates a new file, if none exists.
 #.tell() - returns the current position in bytes
 #.seek(offset,from)
 #readwritefile.truncate()
-with open("file1.txt",'a+') as readwritefile:
+with open("file1.txt",'a+') as readwritefile: #try changing 'a+' with all other above commented characters
     readwritefile.write("Testing the append in the file let's see if it works or not\n")
     
     print(f"Initial Location: {readwritefile.tell()}")
